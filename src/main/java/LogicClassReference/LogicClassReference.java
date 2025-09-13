13ef72f655feffa1419ec1d42114945fba7d59a4
Vojtěch Žáček <vojtech.zacek@olc.cz>
2025-09-13 16:26:49 +0200

    feat(LogicClassReference): find logic class reference by class name instead of filename

--- src/main/java/LogicClassReference/LogicClassReference.java
+++ src/main/java/LogicClassReference/LogicClassReference.java
@@ -9,6 +9,7 @@ import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlText;
+import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -26,18 +27,16 @@ public class LogicClassReference extends PsiReferenceBase<XmlTag> {
     @Override
     public @Nullable PsiElement resolve() {
         Project project = getElement().getProject();
-        Collection<VirtualFile> files = FilenameIndex.getVirtualFilesByName(
-                project,
-                className + ".php",
-                GlobalSearchScope.projectScope(project)
-        );
-        if (files.isEmpty()) return null;
+        if (this.className.isEmpty()) return null;
 
-        return PsiManager.getInstance(project).findFile(files.iterator().next());
+        Collection<? extends PhpClass> classes = PhpIndex.getInstance(project).getAnyByFQN("\\" + className);
+        if (classes.isEmpty()) return null;
+
+        return (PsiElement) classes.toArray()[0];
     }
 
     @Override
     public Object @NotNull [] getVariants() {
-        return new Object[0]; // případně nabídnout completion
+        return new Object[0];
     }
 }
