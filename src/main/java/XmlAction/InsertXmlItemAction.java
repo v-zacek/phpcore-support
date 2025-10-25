84b056a3cb2f80973c62a55e6ad467bd086efdce
Vojtěch Žáček <vojtucha.zacek@seznam.cz>
2025-10-25 21:57:57 +0200

    feat: XML form item code generation

---
+++ src/main/java/XmlAction/InsertXmlItemAction.java
@@ -0,0 +1,63 @@
+package XmlAction;
+
+import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.template.Template;
+import com.intellij.codeInsight.template.TemplateManager;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.xml.XmlFile;
+
+public class InsertXmlItemAction extends AnAction implements DumbAware {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+
+        if (project == null) return;
+        if (editor == null) return;
+
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            TemplateManager templateManager = TemplateManager.getInstance(project);
+            Template template = templateManager.createTemplate("", "");
+            template.setToReformat(true);
+
+            template.addTextSegment("<item html=\"\" type=\"\">\n");
+            template.addTextSegment("    <fieldname></fieldname>\n");
+            template.addTextSegment("    <description></description>\n");
+            template.addTextSegment("    <title></title>\n");
+            template.addTextSegment("    <datatype></datatype>\n");
+            template.addTextSegment("</item>");
+
+            templateManager.startTemplate(editor, template);
+        });
+    }
+
+    public void update(AnActionEvent e) {
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+
+        boolean showAction = editor != null && psiFile instanceof XmlFile && isXmlFile(psiFile);
+        e.getPresentation().setEnabledAndVisible(showAction);
+    }
+
+    private boolean isXmlFile(PsiFile psiFile) {
+        VirtualFile virtualFile = psiFile.getVirtualFile();
+
+        return virtualFile != null && "xml".equalsIgnoreCase(virtualFile.getExtension());
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
+    }
+
+}
